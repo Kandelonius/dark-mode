3432e3f2e52679125c9df29428df6adda5b9be74
Shane Kaestner <shane.kaestner@yahoo.com>
2020-05-06 14:46:26 -0700

    update

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -5,5 +5,9 @@ export const useLocalStorage = (key, initialValue) => {
     const item = window.localStorage.getItem(key);
     return item ? JSON.parse(item) : initialValue;
     });
+    const setValue = value => {
+        setStoredValue(value);
+        window.localStorage.setItem(key, JSON.stringify(value));
+    }// up to this point can be used for any kind of local storage
     return [storedValue];
 };
